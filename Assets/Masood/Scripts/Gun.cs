6e403bfbe21e8cc38742a2e412037a42522d6de4
masood <masood_a_t@yahoo.com>
2024-07-05 00:02:43 +0400

    import

---
+++ Assets/Masood/Scripts/Gun.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public enum GunType
+{
+    None, Pistol, Rifle
+}
+
+public abstract class Gun : MonoBehaviour
+{
+    public PlayerInputActions PlayerControls;
+    public LayerMask ValidLayers;
+    public GunType GunType;
+    protected InputAction Fire;
+    //public float riflevalue;
+
+    [SerializeField] private GameObject _shooptStartPosition;
+    [SerializeField] private Magazine _currentMagazine;
+
+    protected abstract void Initialize();
+    public abstract void DoAction();
+
+
+    private void Awake()
+    {
+        PlayerControls = new PlayerInputActions();
+    }
+    protected void Start()
+    {
+        Initialize();
+    }
+
+
+    private void OnEnable()
+    {
+        Fire = PlayerControls.Player.Fire;
+        Fire.Enable();
+        
+    }
+
+
+
+    private void OnDisable()
+    {
+        Fire.Disable();
+    }
+
+
+    protected void Shoot()
+    {
+        var currentBullet = _currentMagazine.GetBullet();
+        if (currentBullet == null)
+        {
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(_shooptStartPosition.transform.position,
+             transform.forward, out hit, currentBullet.Range,
+             ValidLayers, QueryTriggerInteraction.Ignore))
+            OnRaycastHit(hit, currentBullet.Damage);
+    }
+
+    private void OnRaycastHit(RaycastHit hit, float damage)
+    {
+        Debug.Log(hit.collider.gameObject.name);
+        var damageable = hit.collider.GetComponent<Idamageable>();
+        if (damageable != null)
+            damageable.ReceiveDamage(damage);
+    }
+}
+
+
+
+
---
+++ Assets/Masood/Scripts/Pistol.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Pistol : Gun
+{
+    //[SerializeField] private float _durationBetweenShoot = .2f;
+    [SerializeField] private bool _readyToShoot;
+    //private float _lastShootTime;
+
+    public override void DoAction()
+    {
+        //_lastShootTime = Time.time;
+        //if (!_readyToShoot)
+        //    return;
+        //Shoot();
+        //_readyToShoot = false;
+    }
+
+    protected override void Initialize()
+    {
+        GunType = GunType.Pistol;
+        Fire.performed += Fire_performed;//getkeydown
+    }
+
+    private void Fire_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        //Debug.Log("Performed");
+        Shoot();
+    }
+
+    //private void Update()
+    //{
+    //    //if (Input.GetKey(KeyCode.A))
+    //    if (Fire.ReadValue<float>()>0)
+    //    DoAction();
+
+
+    //    //if (Fire2.IsPressed())//getkey
+    //    //    Debug.Log("IsPressed");
+
+    //    if (Fire2.WasPressedThisFrame())
+    //        Debug.Log("WasPressedThisFrame");
+    //    //riflevalue = Rifle.ReadValue<float>();
+    //    if (Time.time > _lastShootTime + _durationBetweenShoot)
+    //        _readyToShoot = true;
+    //}
+}
+
