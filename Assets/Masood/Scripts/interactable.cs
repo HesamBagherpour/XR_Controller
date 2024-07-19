0945a302a464b9a9dfd8c4157715c685d20474cc
masood <masood_a_t@yahoo.com>
2024-07-19 15:11:03 +0400

    added prefabs

---
+++ Assets/Masood/Scripts/interactable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactable : MonoBehaviour
+{
+public void ShowLog(string message)
+    {
+        Debug.Log(message);
+    }
+}
